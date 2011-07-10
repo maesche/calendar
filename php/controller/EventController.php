2dfc21f06086e5cf9945105dc81dbc51132a1449
Stefan Meier <maesche@me.com>
2011-07-10 22:26:53 +0200

    merge

--- php/controller/EventController.php
+++ php/controller/EventController.php
@@ -5,136 +5,141 @@ include_once("model/EventHandler.php");
 
 class EventController {
 
-    private $room;
-    private $user;
-    private $event;
-    private $timeTable;
-    private $return;
-    private $insertAvailable;
-    private $eventHandler;
+	private $room;
+	private $user;
+	private $event;
+	private $timeTable;
+	private $return;
+	private $insertAvailable;
+	private $eventHandler;
 
-    function __construct($room, $user) {
+	function __construct($room, $user) {
 
-        $this->room = $room;
-        $this->user = $user;
-        $this->return["success"] = true;
-        $this->eventHandler = new EventHandler();
-        $this->insertAvailable = false;
-    }
+		$this->room = $room;
+		$this->user = $user;
+		$this->return["success"] = true;
+		$this->eventHandler = new EventHandler();
+		$this->insertAvailable = false;
+	}
 
-    public function setEvent($event) {
-        $this->event = $event;
-    }
+	public function setEvent($event) {
+		$this->event = $event;
+	}
 
-    public function setInsertAvailable($insertAvailable) {
-        $this->insertAvailable = $insertAvailable;
-    }
+	public function setInsertAvailable($insertAvailable) {
+		$this->insertAvailable = $insertAvailable;
+	}
 
-    public function setTimeTable($timeTable) {
-        $this->timeTable = $timeTable;
-    }
+	public function setTimeTable($timeTable) {
+		$this->timeTable = $timeTable;
+	}
 
-    private function addExclusive() {
-        /*
-         * BEGIN CRITICAL PART
-         *
-         * In this part, only one thread can be handled at once. All other
-         * threads will wait.
-         */
+	private function addExclusive() {
+		/*
+		 * BEGIN CRITICAL PART
+		*
+		* In this part, only one thread can be handled at once. All other
+		* threads will wait.
+		*/
 
-        $file_handle = fopen('../tmp/calendar' . $this->room->getId() . '.lock', 'w+');
-        $finish = false;
-        $insertable = true;
+		$file_handle = fopen('../tmp/calendar' . $this->room->getId() . '.lock', 'w+');
+		$finish = false;
+		$insertable = true;
+		if ($file_handle) {
+			while (!$finish) {
+				if (flock($file_handle, LOCK_EX)) {
+					$dates = null;
 
-        while (!$finish) {
-            if (flock($file_handle, LOCK_EX)) {
-                $dates = null;
+					$verifiedEvents = $this->eventHandler->checkAvailability($this->room, $this->timeTable, $this->event->getDBegin(), $this->event->getLastDate());
 
-                $verifiedEvents = $this->eventHandler->checkAvailability($this->room, $this->timeTable, $this->event->getDBegin(), $this->event->getLastDate());
-  
-                if (count($verifiedEvents["unavailable"]) > 0 && !($this->insertAvailable)) {
-                    $this->return["success"] = false;
-                    $this->return["unavailable"] = $verifiedEvents["unavailable"];
+					if (count($verifiedEvents["unavailable"]) > 0 && !($this->insertAvailable)) {
+						$this->return["success"] = false;
+						$this->return["unavailable"] = $verifiedEvents["unavailable"];
 
-                    $insertable = false;
-                } else {
-                    $dates = $verifiedEvents["available"];
-                }
-                if ($insertable) {
-                    $this->eventHandler->add($this->room, $this->event, $dates);
-                }
+						$insertable = false;
+					} else {
+						$dates = $verifiedEvents["available"];
+					}
+					if ($insertable) {
+						$this->eventHandler->add($this->room, $this->event, $dates);
+					}
 
-                $finish = true;
-            }
-        }
-        fclose($file_handle);
-        /*
-         * END CRITICAL PART
-         */
-    }
+					$finish = true;
+				}
+			}
+			fclose($file_handle);
+		}
+		else {
+			$this->return["success"] = false;
+			$this->return["system"] = "Could not open lock file";
+		}
+		/*
+		 * END CRITICAL PART
+		*/
+	}
 
-    public function action($action) {
-        switch ($action) {
-            case "delete-current" :
-                $this->eventHandler->delete($this->room, $this->event, true);
-                break;
-            case "delete-all" :
-                $this->eventHandler->delete($this->room, $this->event, true, $this->event->getDBegin());
-                break;
-            case "update-current-nodate" :
-                $this->eventHandler->update($this->room, $this->event);
-                break;
-            case "update-current-withdate" :
-                //create new independent event
-                //OK FONCTIONNE
-                $this->addExclusive();
+	public function action($action) {
+		switch ($action) {
+			case "delete-current" :
+				$this->eventHandler->delete($this->room, $this->event, true);
+				break;
+			case "delete-all" :
+				$this->eventHandler->delete($this->room, $this->event, true, $this->event->getDBegin());
+				break;
+			case "update-current-nodate" :
+				$this->eventHandler->update($this->room, $this->event);
+				break;
+			case "update-current-withdate" :
+				//create new independent event
+				//OK FONCTIONNE
+				$this->addExclusive();
 
-                if ($this->return["success"]) {
-                    //delete current item from recurrent lists if new inserted
-                    
-                    $this->eventHandler->delete($this->room, $this->event, $isLast);
+				if ($this->return["success"]) {
+					//delete current item from recurrent lists if new inserted
 
-                }
-                break;
-            case "update-all-nodate" :
-                //$this->eventHandler->update($this->room, $this->event, $this->event->getDBegin());
-                
-                $siblings = $this->eventHandler->getSiblings($this->event, $this->event->getDBegin());
-                
-                $dates = null;
-                
-                foreach($siblings as $sibling) {
-                    $dates[$sibling["date"]] = array("start" => $sibling["start"],
+					$this->eventHandler->delete($this->room, $this->event, $isLast);
+
+				}
+				break;
+			case "update-all-nodate" :
+				//$this->eventHandler->update($this->room, $this->event, $this->event->getDBegin());
+
+				$siblings = $this->eventHandler->getSiblings($this->event, $this->event->getDBegin());
+
+				$dates = null;
+
+				foreach($siblings as $sibling) {
+					$dates[$sibling["date"]] = array("start" => $sibling["start"],
                                                         "end" => $sibling["end"]);
-                     
-                }
+					 
+				}
 
-                $this->eventHandler->update($this->room, $this->event, $dates);
+				$this->eventHandler->update($this->room, $this->event, $dates);
 
-                break;
-            case "update-all-withdate" :
-                //create new independent event
-                
-                $this->addExclusive();
+				break;
+			case "update-all-withdate" :
+				//create new independent event
 
-                if ($this->return["success"]) {
-                    //delete events
-                    $this->eventHandler->delete($this->room, $this->event, true, $this->event->getDBegin());
-                }
-                break;
-            case "add" :
-                $this->addExclusive();
-                break;
-            default :
-                $this->return["success"] = false;
-                $this->return["action"] = $action;
-        }
+				$this->addExclusive();
 
-    }
+				if ($this->return["success"]) {
+					//delete events
+					$this->eventHandler->delete($this->room, $this->event, true, $this->event->getDBegin());
+				}
+				break;
+			case "add" :
+				$this->addExclusive();
+				break;
+			default :
+				$this->return["success"] = false;
+			$this->return["action"] = $action;
+		}
 
-    public function getReturn() {
-        return $this->return;
-    }
+	}
+
+	public function getReturn() {
+		return $this->return;
+	}
 
 }
 
